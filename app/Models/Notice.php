fff7a7c304610a814b4efa2be91c601bacbe0bd8
James Bhatta <jmsbhatta@gmail.com>
2023-10-15 10:50:08 +0545

    Morphable notice, activity and download

--- app/Models/Notice.php
+++ app/Models/Notice.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Notice extends Model
 {
@@ -11,9 +12,9 @@ class Notice extends Model
 
     protected $guarded = [];
 
-    public function committee()
+    public function noticeable(): MorphTo
     {
-        return $this->belongsTo(Committee::class);
+        return $this->morphTo();
     }
 
 }
