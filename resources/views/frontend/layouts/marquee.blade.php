00e584ec860a88c74aeef66505f1042cf2f55c2c
santoshkalukunda <santoshkalukunda@gmail.com>
2023-11-02 10:43:20 +0545

    marquee some change

--- resources/views/frontend/layouts/marquee.blade.php
+++ resources/views/frontend/layouts/marquee.blade.php
@@ -3,6 +3,17 @@
         ताजा अपडेटहरु
     </div>
     <marquee class="bg-white" direction="left" style="padding: 5px; color:brown;" >
-        This is a sample scrolling text that has scrolls texts to left.
+        <a href="" class="mx-4">
+            <i class="bi bi-forward-fill"></i> सभा वैठक स्थगित गरिएको सूचना
+        </a>
+        <a href="" class="mx-4">
+            <i class="bi bi-forward-fill"></i> शोक वक्तव्य
+        </a>
+        <a href="" class="mx-4">
+            <i class="bi bi-forward-fill"></i>  सभा वैठक सम्बन्धी सूचना
+        </a>
+        <a href="" class="mx-4">
+            <i class="bi bi-forward-fill"></i>  दोश्रो अधिवेशन आह्‍वान सम्बन्धी सूचना
+        </a>
     </marquee>
 </div>
