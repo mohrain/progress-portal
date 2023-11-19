da4a2136cb2b16a8b024fb1e0d838b49fff066e6
santoshkalukunda <santoshkalukunda@gmail.com>
2023-11-19 17:45:53 +0545

    old office-bearers

--- resources/views/frontend/office-bearers/old-index.blade.php
+++ resources/views/frontend/office-bearers/old-index.blade.php
@@ -2,16 +2,18 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-9">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="frontend-title">
-                            प्रदेश सभाका पुर्व पदाधिकारीहरु
-                            <hr>
-                        </div>
-                    </div>
-                    @forelse ($officeBearers as $officeBearer)
-                        <div class="col-md-12 my-2">
+
+            <div class="col-md-12">
+                <div class="frontend-title">
+                    प्रदेश सभाका पुर्व पदाधिकारीहरु
+                    <hr>
+                </div>
+            </div>
+            <div class="col-md-6 my-2">
+
+                @forelse ($officeBearers as $officeBearer)
+                    @if ($officeBearer->designation == true)
+                        <div class="my-2">
                             <div class="card">
                                 <div class="row">
                                     <div class="col-md-4">
@@ -24,8 +26,8 @@
                                             class="nav-link text-dark">
                                             <div class="card-body">
                                                 <h5 class="card-title text-theme-color">
-                                                    
-                                                   {{ $officeBearer->member->name }}
+
+                                                    {{ $officeBearer->member->name }}
                                                 </h5>
                                                 <div class="cart-text">
                                                     <b>
@@ -46,16 +48,61 @@
                                 </div>
                             </div>
                         </div>
-                    @empty
-                        <div class="font-italic text-center">
-                            कुनैपनि डाटा उपलब्ध छैन !!!
+                    @endif
+                @empty
+                    <div class="font-italic text-center">
+                        कुनैपनि डाटा उपलब्ध छैन !!!
+                    </div>
+                @endforelse
+            </div>
+            <div class="col-md-6 my-2">
+
+                @forelse ($officeBearers as $officeBearer)
+                    @if ($officeBearer->designation == false)
+                        <div class="my-2">
+                            <div class="card">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <img id="newProfilePhotoPreview"
+                                            src="{{ $officeBearer->member->profile ? asset('storage/' . $officeBearer->member->profile) : asset('assets/img/no-image.png') }}"
+                                            class="feature-image card-img-top">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <a href="{{ route('members.show', $officeBearer->member) }}"
+                                            class="nav-link text-dark">
+                                            <div class="card-body">
+                                                <h5 class="card-title text-theme-color">
+
+                                                    {{ $officeBearer->member->name }}
+                                                </h5>
+                                                <div class="cart-text">
+                                                    <b>
+                                                        {{ $officeBearer->designation == true ? 'सभामुख' : 'उप सभामुख' }}
+                                                    </b>
+                                                    @if ($officeBearer->end)
+                                                        <div>
+                                                            <small>
+                                                                <b>कार्यकालः </b> {{ $officeBearer->start }} देखि
+                                                                {{ $officeBearer->end }} सम्म
+                                                            </small>
+                                                        </div>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endforelse
-                </div>
+                    @endif
+
+                @empty
+                    <div class="font-italic text-center">
+                        कुनैपनि डाटा उपलब्ध छैन !!!
+                    </div>
+                @endforelse
             </div>
-            <div class="col-md-3">
-                @include('frontend.layouts.sidebar')
-            </div>
+
         </div>
     </div>
 @endsection
