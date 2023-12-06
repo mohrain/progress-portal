a8b17ef6578bceb5b00213378c7a029ab6b1443f
santoshkalukunda <santoshkalukunda@gmail.com>
2023-12-06 12:59:07 +0545

    side bar remove

--- resources/views/frontend/post-category/show.blade.php
+++ resources/views/frontend/post-category/show.blade.php
@@ -44,7 +44,7 @@
                     </form>
                 </div>
             </div>
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <div class="row">
                     <div class="col-md-12">
                         <table class="table table-striped table-hover box p-2">
