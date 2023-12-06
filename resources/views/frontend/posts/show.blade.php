480ec0aa0eb87b0845679495daf261241f0a5d6b
santoshkalukunda <santoshkalukunda@gmail.com>
2023-12-06 13:24:57 +0545

    post show sidebar

--- resources/views/frontend/posts/show.blade.php
+++ resources/views/frontend/posts/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-9">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="frontend-title">
@@ -55,9 +55,9 @@
                     @endif
                 </div>
             </div>
-            {{-- <div class="col-md-3">
+            <div class="col-md-3">
                 @include('frontend.layouts.sidebar')
-            </div> --}}
+            </div>
         </div>
     </div>
 @endsection
